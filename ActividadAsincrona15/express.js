f092c94c16af3ba2a6cedae55f0b0e4890d27210
jhonastba94 <109564287+jhonastba94@users.noreply.github.com>
2022-12-21 11:11:15 -0500

    Update express.js

--- ActividadAsincrona15/express.js
+++ ActividadAsincrona15/express.js
@@ -1,23 +1,5 @@
 
-// const http=require('http')
-
-// const hostname = '127.0.0.1'
-// const port = 3080
-// //funcion
-// const server = http.createServer((request, response)=>{
-//     response.statusCode = 200; // 200 HTTP response status codes captura el eeror si lo hay(error 404 por ejemplo)
-//     response.setHeader('Content-Type', 'text/plain')
-//     response.end('Jhon Jhonatan ASTUCURI BARRIAL')
-// })
- 
-// server.listen(port, hostname, () => { 
-//     console.log(`El servidor se esta ejecutando en http://${hostname}:${port}/`)
-// })
-
-
-
-
- const express = require('express')
+const express = require('express')
 const app = express()
 const port = 3000
 app.get('/', (request, response)=>{ //Metodo get para conectarese al servidor
