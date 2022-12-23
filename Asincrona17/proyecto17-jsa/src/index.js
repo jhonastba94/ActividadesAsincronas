6b09c499318d7444f9a0c346580c4466d03bef40
jhonastba94 <109564287+jhonastba94@users.noreply.github.com>
2022-12-23 10:13:44 -0500

    Add files via upload

---
+++ Asincrona17/proyecto17-jsa/src/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const userRoute = require("./routes/user");
+
+mongoose.set('strictQuery', true);
+
+
+const app = express();
+const port = process.env.PORT || 9000;
+
+
+
+// middlewares
+app.use(express.json());
+app.use("/api", userRoute);
+
+
+
+// routes
+app.get("/", (req, res) => {
+    res.send("Welcome to my API");
+  });
+
+// mongodb connection
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch((error) => console.error(error));
+  
+app.listen(port, () => console.log("Server listening to", port));
+
+
